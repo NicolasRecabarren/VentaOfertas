d3016f73ec6036834223f14fbcaa2a00769c9521
DESKTOP-3028CMM\recab <np.recabarren.v@gmail.com>
2020-12-01 22:57:40 -0300

    Fix de puerto para heroku.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import app from './app';
 
 async function runServer(){
-    const port = process.env.PORT || 80;
+    const port = process.env.PORT || 3000;
+    console.log(`Puerto: ${port}`);
     await app.listen(port);
     console.log(`Server on port ${port}`);
 };
