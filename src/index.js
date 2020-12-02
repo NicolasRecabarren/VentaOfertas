36faa331aeb01a4fe9fa6d4a9e75c479325cda03
DESKTOP-3028CMM\recab <np.recabarren.v@gmail.com>
2020-12-01 23:26:39 -0300

    Fix para heroku.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import app from './app';
 
+const port = process.env.PORT || 3000;
+
 async function runServer(){
-    console.log(process.env);
-    const port = process.env.PORT || 3000;
     await app.listen(port);
     console.log(`Server on port ${port}`);
 };
